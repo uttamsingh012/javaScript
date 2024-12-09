d07e44fb1ab59a1c2864662d6ff92ce6f7abbf9f
uttamsingh012 <115942682+uttamsingh012@users.noreply.github.com>
2024-12-09 13:18:54 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs, not browser
+
+console.log(3+3); console.log("Uttam")
+
+let name = "Uttam"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standardlone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
