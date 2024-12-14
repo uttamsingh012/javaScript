252faaa1b106a7c3c31aec1957c51fcbe1259fb2
uttamsingh012 <115942682+uttamsingh012@users.noreply.github.com>
2024-12-14 14:14:44 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -5,6 +5,20 @@
     console.log("DB CONNECTED")
 }) ();
 
+
+
 ( (name) => {
     console.log(`DB CONNECTED  TO ${name}`)
 } ) ("uttam");
+
+
+let val1 = 10
+let val2 = 5
+function sum(num1, num2){
+    let total = num1 + num2
+    return total
+}
+let result1 = sum(val1, val2)
+let result2 = sum(3, 5)
+console.log(result1);
+console.log(result2);
